1ceca124c65bff4357ce362365a61f930f71b5ae
Mohamed.AbdelLatif <Mohamed.AbdelLatif@EBS10-HPLP-52>
2025-01-09 10:46:19 +0200

    Add project files.

---
+++ Home3D/Controllers/DevicesController.cs
@@ -0,0 +1,23 @@
+﻿using Home3D.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Home3D.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DevicesController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetDevices()
+    {
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "JsonData", "Devices.json");
+        if (!System.IO.File.Exists(filePath))
+            return NotFound("JSON file not found.");
+
+        var jsonData = await System.IO.File.ReadAllTextAsync(filePath);
+        var devices = JsonSerializer.Deserialize<List<Device>>(jsonData);
+        return Ok(devices);
+    }
+}
---
+++ Home3D/Models/Device.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Home3D.Models;
+
+public class Device
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("latitude")]
+    public double Latitude { get; set; }
+
+    [JsonPropertyName("longitude")]
+    public double Longitude { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+}
